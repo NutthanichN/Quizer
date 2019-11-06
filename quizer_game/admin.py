b19c31cf3b194852e34a5bb50b1c4282370a93b2
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-07 01:28:01 +0700

    Add quizs, questions, choices in the admin site

--- quizer_game/admin.py
+++ quizer_game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Quiz, Question, Choice
+
 # Register your models here.
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Choice)
