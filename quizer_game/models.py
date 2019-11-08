dd1ed3e91a76751e2b9eb7a7d32f711da35017d9
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-08 17:50:08 +0700

    Add status attributes to player and add method to quiz

--- quizer_game/models.py
+++ quizer_game/models.py
@@ -13,6 +13,17 @@ class Quiz(models.Model):
     def __str__(self):
         return self.topic
 
+    def active_player(self):
+        pass
+
+    def find_active_player(self):
+        pass
+
+    def create_player(self, name, selected_difficulty):
+        player = self.player_set.create(name=name, selected_difficulty=selected_difficulty, is_playing=True)
+        player.save()
+        return player
+
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
@@ -33,13 +44,26 @@ class Choice(models.Model):
 
 class Player(models.Model):
     # game difficulty: easy(0) medium(1) hard(2)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
     time = models.TimeField(null=True, blank=True)
     selected_difficulty = models.IntegerField(default=0)
     position = models.IntegerField(default=0)
+    is_playing = models.BooleanField(default=False)
+    is_failed = models.BooleanField(default=False)
+    is_achieved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
-    def move(self):
-        pass
+    def move_forward(self):
+        self.position += 1
+        self.save()
+
+    def move_backward(self):
+        self.position -= 1
+        self.save()
+
+    def finish_playing(self):
+        self.is_playing = False
+        self.save()
