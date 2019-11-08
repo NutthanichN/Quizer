56ccee1d4ad49aa14c81418a705fb03924de98fe
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-08 17:54:00 +0700

    Display player, quiz and its question and choices on game.html

--- quizer_game/urls.py
+++ quizer_game/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     # /quizer/
     path('', views.test_index, name='test_index'),
     # /quizer/game/1/
-    path('game/<int:quiz_id>', views.game, name='game'),
+    # path('game/<int:quiz_id>/<int:player_id>/', views.game, name='game'),
+    # /quizer/game/player-name/
+    path('test-player-name/', views.run_test_player_name_template, name='test-player-name'),
+    # /quizer/game/player-name/select-quiz/
+    path('test-select-quiz/<str:player_name>/', views.run_test_select_quiz_template, name='test-select-quiz'),
+    # /quizer/game/player_id/quiz_id/difficulty/question_id/
+    path('start-game/<str:player_name>/<int:quiz_id>/<int:selected_difficulty>/<int:question_id>/',
+         views.start_game, name='start-game'),
 ]
+
+# player_name, quiz_id, selected_difficulty
--- quizer_game/views.py
+++ quizer_game/views.py
@@ -11,14 +11,38 @@ def test_index(request):
     return HttpResponse("Hello, world. You're at the Quizer game test index.")
 
 
-def game(request, quiz_id):
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    return render(request, 'quizer_game/game.html',
-                  {'quiz': quiz})
-    # response = "Game of Quiz %s."
-    # return HttpResponse(response % quiz_id)
+# def game(request, quiz_id):
+#     quiz = get_object_or_404(Quiz, pk=quiz_id)
+#     return render(request, 'quizer_game/game.html',
+#                   {'quiz': quiz})
+#     # response = "Game of Quiz %s."
+#     # return HttpResponse(response % quiz_id)
 
 # class GameView()
+
+# /quizer/game/player_name/quiz_id/difficulty/question_id/
+def start_game(request, player_name, quiz_id, selected_difficulty, question_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    # test with player 1
+    player = quiz.player_set.get(pk=1)
+    # create new player
+    # player = quiz.create_player(player_name, selected_difficulty)
+    # player = quiz.player_set.create(name='player2', selected_difficulty=0, is_playing=True)
+    question = quiz.question_set.get(pk=question_id)
+    context = {'quiz': quiz,
+               'player': player,
+               'question': question,
+               }
+    return render(request, 'quizer_game/game.html', context)
+
+
+def run_test_select_quiz_template(request, player_name):
+    context = {'player_name': player_name}
+    return render(request, 'quizer_game/test-select-quiz.html', context)
+
+
+def run_test_player_name_template(request):
+    return render(request, 'quizer_game/test-player-name.html')
 
 
 def answer(request):
