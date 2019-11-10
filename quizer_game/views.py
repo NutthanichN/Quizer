8d20fde81dce7bd9c3bdc7d5b4a7047ceb1d1d4f
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-10 21:06:39 +0700

    Fix bug when config next qustion to player

--- quizer_game/views.py
+++ quizer_game/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views import generic
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Quiz
 
 # Create your views here.
+DIFFICULTY = {'easy': 0, 'medium': 1, 'hard': 2}
+CHOICE_VALUE = {'wrong': 0, 'correct': 1}
+POSITION = {'max': 15, 'min': 0}
 
 
 def test_index(request):
@@ -15,20 +18,27 @@ def test_index(request):
 # /quizer/start-game/player_name/quiz_id/difficulty/
 def start_game(request, player_name, quiz_id, selected_difficulty):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
-    # questions = list(quiz.question_set.all())
     # test with player 1
-    player = quiz.player_set.get(pk=1)
-    player.current_question = quiz.question_set.get(number=1)
-    player.position = 11
-    # player.selected_difficulty = selected_difficulty
-    player.save()
-    # create new player
+    player = setup_player_for_testing(quiz, 1, DIFFICULTY['easy'], POSITION['min'])
+
+    # (real) create new player
     # player = quiz.create_player(player_name, selected_difficulty)
 
     return redirect(reverse('quizer_game:game',
-                            kwargs={'player_id': 1, 'quiz_id': quiz_id,
-                                    'selected_difficulty': selected_difficulty,
-                                    }))
+                            kwargs={'player_id': player.id, 'quiz_id': quiz_id,
+                                    'selected_difficulty': player.selected_difficulty,
+                                    }
+                            )
+                    )
+
+
+def setup_player_for_testing(quiz, player_id, selected_difficulty, position):
+    player = quiz.player_set.get(pk=player_id)
+    player.current_question = quiz.question_set.get(number=1)
+    player.position = position
+    player.selected_difficulty = selected_difficulty
+    player.save()
+    return player
 
 
 # /quizer/game/player_id/quiz_id/difficulty/
@@ -44,16 +54,9 @@ def game(request, player_id, quiz_id, selected_difficulty):
     return render(request, 'quizer_game/game.html', context)
 
 
-DIFFICULTY = {'easy': 0, 'medium': 1, 'hard': 2}
-CHOICE_VALUE = {'wrong': 0, 'correct': 1}
-POSITION = {'max': 15, 'min': 0}
-QUESTION_NUM = {'max': 20}
-
-
 # TODO edit redirect to the real template
 # TODO manage player time spent and set limit time for the hard level
 # TODO provide upvote-downvote feature
-# TODO manage admit page (so we don't have to create quiz by API)
 # /quizer/game/player_id/quiz_id/difficulty/update/
 def update_game(request, player_id, quiz_id, selected_difficulty, choice_value):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
@@ -75,12 +78,11 @@ def update_game(request, player_id, quiz_id, selected_difficulty, choice_value):
         return redirect(reverse('quizer_game:test-result-achieve'))
     elif player.position < POSITION['max']:
         # change to next question
-        if player.current_question.number < QUESTION_NUM['max']:
+        try:
             old_question = player.current_question
             new_question_number = old_question.number + 1
             player.current_question = quiz.question_set.get(number=new_question_number)
-        # check when question num = 20 but player doesn't reach the finish line
-        elif player.current_question.number == QUESTION_NUM['max']:
+        except ObjectDoesNotExist:
             player.is_failed = True
             player.is_playing = False
             player.save()
