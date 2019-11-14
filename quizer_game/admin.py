a8a2299548fc27e9c70bd4f1bdb9a3796cbc5933
Nutthanich Narphromar <48724051+NutthanichN@users.noreply.github.com>
2019-11-14 20:09:18 +0700

    Merge main game template to master branch (#15)
    * Add game template
    
    * Add quit button and fix overflow in question body
    
    * Create Quiz, Question, Choice, and Player models
    
    * Map urls and views (with test index)
    
    * Edit Quiz's attribute name
    
    * Add quizs, questions, choices in the admin site
    
    * Create urls to game page (test version)
    
    * Move quit button to the top right corner and display quiz topic from views
    
    * Add status attributes to player and add method to quiz
    
    * Add empty test file
    
    * Display player, quiz and its question and choices on game.html
    
    * Car img moves when press a correct choice
    
    * Redirect to result page when player reaches the finish line (test ver.)
    
    * Display next question
    
    * Add TODO comments
    
    * Configure list views and list filters on admin site
    
    * Change timezone
    
    * Add inline editing of associated records on admin page
    
    * Add link to edit each question via quiz admin on admin page
    
    * Create and config Player in Quiz
    
    * Fix bug when config next qustion to player
    
    * Refactor views for easily manual testing
    
    * Update base master (merge master to this branch)
    
    * Add dumpdata for manual testing
    
    * Update base master (merge updated master to this branch)
    
    * Update base master (merge updated master to this branch)
    
    * Update base master (merge edit-question-set template from master to this branch
    
    * Update base master (merge question-set-detail template to this branch)
    
    * Create Player at view instead of Quiz model
    
    * Add test models
    
    * Add empty folder test_views
    
    * Change class name
    
    * Remove notation
    
    * Add test game view
    
    * Update base master (merge templates to this branch)
    
    * Add real urls and views for game system
    
    * Update base master (merge templates to this branch)
    
    * Set Test Quiz contain 20 questions on template for manual testing
    
    * Fix merge conflicts
    
    * Accept change from master (try to resolve merge conflicts)
    
    * Add urls for manual testing

--- quizer_game/admin.py
+++ quizer_game/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
+from .models import Quiz, Question, Choice, Player
+
 # Register your models here.
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    show_change_link = True
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('topic', )
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('number', 'text', 'quiz')
+    list_filter = ('quiz',)
+    list_display_links = ('text', )
+    inlines = [ChoiceInline]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'value', 'question')
+    list_filter = ('question',)
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quiz', 'is_achieved', 'is_failed', 'time')
+    list_filter = ('name', 'quiz', 'is_achieved', 'is_failed')
+    fields = ['name', ('quiz', 'selected_difficulty'), ('current_question', 'position'),
+              ('is_playing', 'is_achieved', 'is_failed')]
