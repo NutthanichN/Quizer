9f82ac23691b466e9f8d4f1631a5bcb72f31dfd7
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-26 01:01:49 +0700

    Change docstring style

--- quizer_game/tests/test_models/test_quiz_model.py
+++ quizer_game/tests/test_models/test_quiz_model.py
@@ -10,13 +10,17 @@ class QuizModelTest(TestCase):
         Quiz.objects.create(topic='Python Programming')
 
     def test_topic_label(self):
-        """Test verbose name of topic (CharField)"""
+        """
+        Test verbose name of topic (CharField)
+        """
         quiz = Quiz.objects.get(id=1)
         field_label = quiz._meta.get_field('topic').verbose_name
         self.assertEquals(field_label, 'Topic')
 
     def test_topic_max_length(self):
-        """Test max length of topic (CharField)"""
+        """
+        Test max length of topic (CharField)
+        """
         quiz = Quiz.objects.get(id=1)
         field_max_length = quiz._meta.get_field('topic').max_length
         self.assertEquals(field_max_length, 200)
