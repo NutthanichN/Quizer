1c5e82c0c4f9207505f9630f43050d7dd4795962
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-28 19:53:16 +0700

    Fix bugs from merge (#18)

--- quizer_game/urls.py
+++ quizer_game/urls.py
@@ -7,19 +7,20 @@ app_name = 'quizer_game'
 urlpatterns = [
     # /quizer/
     path('', views.index, name='index'),
-    path('select-player-name/', views.player_name, name='player-name'),
-    path('login/', views.login, name='login'),
-    path('leaderboard-index/', views.leaderboard_index, name='leaderboard-index'),
-    path('<str:player_name>/quiz-level/', views.quiz_level, name='quiz-level'),
-    path('start-game/<str:player_name>/<int:quiz_id>/<int:selected_difficulty>/',
+    path('select-player-name/',
+         views.player_name, name='player-name'),
+    path('login/',
+         views.login, name='login'),
+    path('leaderboard-index/',
+         views.leaderboard_index, name='leaderboard-index'),
+    path('quiz-level/',
+         views.quiz_level, name='quiz-level'),
+    path('start-game/<str:player_name>/',
          views.start_game, name='start-game'),
-
     # /quizer/game/player_id/quiz_id/difficulty/question_id/
     path('game/<int:player_id>/<int:quiz_id>/<int:selected_difficulty>/',
          views.game, name='game'),
-
-    # /quizer/game/player_id/quiz_id/difficulty/choice_value/update/
-    path('game/<int:player_id>/<int:quiz_id>/<int:selected_difficulty>/<int:choice_value>/update/',
+    path('game/<int:player_id>/<int:quiz_id>/<int:selected_difficulty>/update/',
          views.update_game, name='update'),
     path('game/<int:player_id>/<int:quiz_id>/<int:selected_difficulty>/result/',
          views.result, name='result'),
