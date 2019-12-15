7e5e88fdf8e06b9983daf295ad0085af9925b6bf
Nutthanich Narphromar <monaprom134@gmail.com>
2019-12-16 02:29:29 +0700

    Add quizer_game/static to path

--- quizer_site/settings.py
+++ quizer_site/settings.py
@@ -149,12 +149,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'quizer_game/static'),
 )
 
 
