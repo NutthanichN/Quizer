d80528b778e20a1a313ea7cf2b79ff0bcf4b8149
Nutthanich Narphromar <monaprom134@gmail.com>
2019-12-16 02:18:02 +0700

    Add STATIC_TMP

--- quizer_site/settings.py
+++ quizer_site/settings.py
@@ -149,6 +149,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
