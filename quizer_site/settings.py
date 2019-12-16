59e87ebe18f617aa6df87be95c6c95d1f54b4154
Nutthanich Narphromar <monaprom134@gmail.com>
2019-12-16 10:12:35 +0700

    Set STATICFILES_DIRS

--- quizer_site/settings.py
+++ quizer_site/settings.py
@@ -149,11 +149,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'quizer_game', 'static'),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'quizer_game', 'static'),]
 
-STATICFILES_STORAGE = 'whilenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whilenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGGING_CONFIG = None
