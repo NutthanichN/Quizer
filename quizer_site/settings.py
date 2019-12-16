5c91771be6331756604cb3bb850e8bdf585ff0b9
Nutthanich Narphromar <monaprom134@gmail.com>
2019-12-16 08:06:27 +0700

    Fix path at STATICFILES_DIRS

--- quizer_site/settings.py
+++ quizer_site/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'quizer_game/static'),
+    os.path.join(BASE_DIR, 'quizer_game', 'static'),
 )
 
 
