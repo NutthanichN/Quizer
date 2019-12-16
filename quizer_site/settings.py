f80702d22777b7e193cf0af9441033c046eb2b86
Nutthanich Narphromar <monaprom134@gmail.com>
2019-12-16 10:18:34 +0700

    Try to fix STATIC_ROOT

--- quizer_site/settings.py
+++ quizer_site/settings.py
@@ -149,10 +149,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'quizer_game', 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'quizer_game', 'static'), ]
 
-# STATICFILES_STORAGE = 'whilenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGGING_CONFIG = None
 logging.config.dictConfig({
