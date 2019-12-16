a7fac0f468ee26ece18358220713c993987a3087
Nutthanich Narphromar <monaprom134@gmail.com>
2019-12-16 13:20:10 +0700

    Try set debug = true

--- quizer_site/settings.py
+++ quizer_site/settings.py
@@ -34,7 +34,8 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = config('SECRET_KEY', default="secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+# DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 # Store the extracted extra data
 SOCIAL_AUTH_POSTGRES_JSONFIELD = True
